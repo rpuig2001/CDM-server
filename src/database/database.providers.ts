89af9c88ddcbcb375e248bfbe058c46f1a3f5735
rpuig11 <rpuig2001@gmail.com>
2024-05-08 15:04:48 +0200

    MongoDB updated

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -4,7 +4,9 @@ export const databaseProviders = [
   {
     provide: 'DATABASE_CONNECTION',
     useFactory: async (): Promise<typeof mongoose> =>
-      //await mongoose.connect('mongodb://mongo:YvbYlIWABwmAWEoQWPYfoFuiyLUkHiYt@monorail.proxy.rlwy.net:55209',),
-      await mongoose.connect('mongodb+srv://cdm_admin:ugR6h4EopPJdGZUZ@cdm-cluster.qhmhmcu.mongodb.net/interface?retryWrites=true&w=majority&appName=CDM-Cluster',),
+      await mongoose.connect(
+        'mongodb://mongo:YvbYlIWABwmAWEoQWPYfoFuiyLUkHiYt@monorail.proxy.rlwy.net:55209',
+      ),
+    //await mongoose.connect('mongodb+srv://cdm_admin:ugR6h4EopPJdGZUZ@cdm-cluster.qhmhmcu.mongodb.net/interface?retryWrites=true&w=majority&appName=CDM-Cluster',),
   },
 ];
