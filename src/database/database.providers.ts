0bdf19a17c1f7eb1757d0c4071211e4e6f681509
Roger Puig <68125167+rpuig2001@users.noreply.github.com>
2024-05-26 00:47:58 +0200

    Update database.providers.ts

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -4,13 +4,10 @@ export const databaseProviders = [
   {
     provide: 'DATABASE_CONNECTION',
     useFactory: async (): Promise<typeof mongoose> =>
-      await mongoose.connect(
-        'mongodb://mongo:IEtDXdDbODrgdypXYxrzRMZzfYENNqxK@monorail.proxy.rlwy.net:26672',
-        {
-            uri_decode_auth: true,
-            useUnifiedTopology: true,
-          }
-      ),
+      await mongoose.connect('mongodb://mongo:IEtDXdDbODrgdypXYxrzRMZzfYENNqxK@monorail.proxy.rlwy.net:26672', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      } as ConnectOptions),
     /*await mongoose.connect(
         'mongodb+srv://cdm_admin:ugR6h4EopPJdGZUZ@cdm-cluster.qhmhmcu.mongodb.net/interface?retryWrites=true&w=majority&appName=CDM-Cluster',
       ),*/
