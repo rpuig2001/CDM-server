845bcd9b9b6f5d8e566a29cb740f5d6b3c204e02
Roger Puig <68125167+rpuig2001@users.noreply.github.com>
2024-05-26 01:07:50 +0200

    App service optimizations

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,25 +1,43 @@
-import { Injectable } from '@nestjs/common';
-import { Cron } from '@nestjs/schedule';
-import axios from 'axios';
+import { Injectable, HttpService, Logger } from '@nestjs/common';
+import { Cron, CronExpression } from '@nestjs/schedule';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 @Injectable()
 export class AppService {
+  private readonly logger = new Logger(AppService.name);
+
+  constructor(private httpService: HttpService) {}
+
   getInitialize(): string {
     return 'CDM API';
   }
 
-  @Cron('*/5 * * * *')
+  @Cron(CronExpression.EVERY_5_MINUTES)
   async handleCronProcess() {
     try {
-      console.log(`Request sent to start calculation`);
-      await axios.post(
-        'https://cdm-server-production.up.railway.app/slotService/process',
-      );
-      await axios.post(
-        'https://cdm-server-production.up.railway.app/slotService/calculate',
-      );
+      this.logger.log('Sending request to start calculation');
+
+      // Send request to start calculation
+      await this.sendRequest('/slotService/process');
+
+      // Send request to perform calculation
+      await this.sendRequest('/slotService/calculate');
+
+      this.logger.log('Requests successfully sent.');
     } catch (error) {
-      console.error(`HTTP request failed: ${error.message}`);
+      this.logger.error(`HTTP request failed: ${error.message}`);
     }
+  }
+
+  private async sendRequest(endpoint: string) {
+    return this.httpService
+      .post(`https://cdm-server-production.up.railway.app${endpoint}`, {})
+      .pipe(
+        catchError((error) => {
+          return throwError(() => error);
+        }),
+      )
+      .toPromise();
   }
 }
