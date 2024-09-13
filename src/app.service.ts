55469a70ddfa17a5bd03d5f0bab961a8eb0a6738
rpuig11 <rpuig2001@gmail.com>
2024-09-13 10:02:36 +0200

    Revert "Disabled cron job"
    This reverts commit 6b7592a1855152abda9602c4a068e08f9ba6c33e.

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { Cron } from '@nestjs/schedule';
+import axios from 'axios';
 
 @Injectable()
 export class AppService {
   getInitialize(): string {
     return 'CDM API';
+  }
+
+  @Cron('*/5 * * * *')
+  async handleCronProcessAndCalculate() {
+    try {
+      /*console.log(`Request sent to start calculation`);*/
+      await axios.post(
+        'https://cdm-server-production.up.railway.app/slotService/process',
+      );
+      await axios.post(
+        'https://cdm-server-production.up.railway.app/slotService/calculate',
+      );
+    } catch (error) {
+      console.error(`HTTP request failed: ${error.message}`);
+    }
+  }
+
+  @Cron('*/1 * * * *')
+  async handleCronProcessForMasterAirports() {
+    try {
+      await axios.post(
+        'https://cdm-server-production.up.railway.app/airport/removedUnusedMasters',
+      );
+    } catch (error) {
+      console.error(`HTTP request failed: ${error.message}`);
+    }
   }
 }
